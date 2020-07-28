4b968fe95e59e95ed3526772851fd721a719b8ce
GGYU720 <57284584+mghan720@users.noreply.github.com>
2020-07-29 01:15:32 +0900

    Revert "fix"
    This reverts commit e9d2d4faa74f8c8e158ecfa82b5ce7afefd0975b.

--- exporter.py
+++ exporter.py
@@ -4,7 +4,7 @@ import csv
 def save_to_file(jobs):
     file = open("jobs.csv", mode="w", encoding="utf-8")
     writer = csv.writer(file)
-    writer.writerow(["Title", "Company", "Location", "Link"])
+    writer.writerow(["title", "company", "location", "link"])
     for job in jobs:
         writer.writerow(list(job.values()))
     return
--- main.py
+++ main.py
@@ -44,4 +44,4 @@ def export():
         return redirect("/")
 
 
-app.run(host="0.0.0.0")
+app.run(host="127.0.0.1")
